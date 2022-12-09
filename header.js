e72f4e8d0f256850917adba82f0c94f13ea92fce
dylan-dot-c <78489976+dylan-dot-c@users.noreply.github.com>
2022-12-09 16:14:08 -0500

    Add files via upload

---
+++ header.js
@@ -0,0 +1,17 @@
+const header = document.getElementById('nav');
+
+header.innerHTML =
+ `<div>
+    <img src="/images/logo.png" alt="">
+    </div>
+    <div class="links">
+    <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li><a href="aboutus.html">About</a> </li>
+        <li><a href="contachus.html">Contact</a></li>
+    </ul>
+    </div>
+    <div>
+    <button>Sign Up</button>
+</div>`
